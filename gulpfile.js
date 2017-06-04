5ca9a9ed6dba2de5f940b4146d0bfe443f0edf3c
Marat Nuriev <nurieff@gmail.com>
2017-06-05 00:42:59 +0500

    initial commit fot github pages

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function () {
-    return gulp.src('./src/table-fits.scss')
+    return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./css'));
 });
