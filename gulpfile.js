cd6f51f1bfdc36923b0541e85b5fcdca793e922f
Marat Nuriev <nurieff@gmail.com>
2019-08-20 14:00:03 +0500

    new pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,47 @@
-let rollup = require('rollup-stream');
-let source = require('vinyl-source-stream');
+let rollup = require('rollup');
 let gulp = require('gulp');
 let sass = require('gulp-sass');
+let pkg = require('./package.json');
+let buble = require('rollup-plugin-buble');
+let uglify = require('rollup-plugin-uglify').uglify;
+let minify = require('uglify-es');
 
 const isProd = process.env.BUILD === 'production';
 
+let plugins = [
+  buble()
+];
+
+if (isProd) {
+  plugins.push(uglify({}, minify));
+}
+
 gulp.task('scss', () => {
-    return gulp.src('./assets/scss/**/*.scss')
-        .pipe(sass()
-            .on('error', sass.logError)
-        )
-        .pipe(gulp.dest('./css'));
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass()
+      .on('error', sass.logError)
+    )
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('js', () => {
-    return rollup('rollup.config.js')
-        .pipe(source('app.js'))
-        .pipe(gulp.dest('./js'));
+  return rollup.rollup({
+    input: './assets/js/app.js',
+    plugins: plugins
+  }).then(bundle => {
+    return bundle.write({
+      file: 'js/app.js',
+      format: 'iife',
+      name: 'App',
+      sourcemap: !isProd
+    });
+  });
 });
 
-gulp.task('watch', () => {
-    gulp.watch('./assets/scss/**/*.scss', ['scss']);
-    gulp.watch('./assets/js/**/*.js', ['js']);
-});
+gulp.task('watch', gulp.parallel('js', 'scss', (done) => {
+  gulp.watch('./assets/scss/**/*.scss', gulp.series('scss'));
+  gulp.watch('./assets/js/**/*.js', gulp.series('js'));
+  done();
+}));
 
-gulp.task('default', ['js', 'scss']);
+gulp.task('default', gulp.parallel('js', 'scss'));
--- js/app.js
+++ js/app.js
@@ -1,1 +1,103 @@
-var App=function(){"use strict";var t=function(t,n){this._config={callback:null},this._config=Object.assign({},this._config,n),this._el="string"==typeof t?document.querySelector(t):t,this._maxWidth=parseInt(getComputedStyle(this._el).width,10),this._elCS=getComputedStyle(this._el),this._container=null,this._hand=null,this._init()};t.make=function(n,e){return new t(n,e)},t.prototype._init=function(){var t=this._elCS.position;-1===["fixed","relative","absolution"].indexOf(t)&&(this._el.style.position="relative"),this._createHand(),this._el.appendChild(this._container)},t.prototype._createHand=function(){this._container=document.createElement("div"),this._container.classList.add("drag-block"),this._hand=document.createElement("div"),this._hand.classList.add("drag-block__hand"),this._container.appendChild(this._hand),this._hand.onmousedown=this._handDown.bind(this)},t.prototype._handDown=function(t){var n=this,e=parseInt(getComputedStyle(this._el).width,10),i=t.pageX;document.body.classList.add("noselect"),document.body.onmousemove=function(t){var o=e-(i-t.pageX);o<.1*n._maxWidth&&(o=Math.round(.1*n._maxWidth)),o>n._maxWidth&&(o=n._maxWidth),n._el.style.width=o+"px",n._config.callback&&n._config.callback()},document.body.onmouseup=function(){document.body.classList.remove("noselect"),document.body.onmouseup=document.body.onmousemove=null}};var n=function(){};return n.DragBlock=t,n}();
+var App = (function () {
+    'use strict';
+
+    var App_DragBlock = function App_DragBlock(el,cfg) {
+
+        this._config = {
+            callback: null
+        };
+
+        this._config = Object.assign({},this._config, cfg);
+
+        /**
+         * @type {HTMLDivElement}
+         * @private
+         */
+        this._el = typeof el === 'string' ? document.querySelector(el) : el;
+
+        this._maxWidth = parseInt(getComputedStyle(this._el).width,10);
+
+        /**
+         * @type {CSSStyleDeclaration}
+         * @private
+         */
+        this._elCS = getComputedStyle(this._el);
+
+        /**
+         * @type {HTMLDivElement}
+         * @private
+         */
+        this._container = null;
+
+        /**
+         * @type {HTMLDivElement}
+         * @private
+         */
+        this._hand = null;
+
+        this._init();
+    };
+
+    App_DragBlock.make = function make (el,cfg) {
+        return new App_DragBlock(el,cfg);
+    };
+
+    App_DragBlock.prototype._init = function _init () {
+        var p = this._elCS.position;
+
+        if (['fixed', 'relative', 'absolution'].indexOf(p) === -1) {
+            this._el.style.position = 'relative';
+        }
+
+        this._createHand();
+
+        this._el.appendChild(this._container);
+    };
+
+    App_DragBlock.prototype._createHand = function _createHand () {
+        this._container = document.createElement('div');
+        this._container.classList.add('drag-block');
+
+        this._hand = document.createElement('div');
+        this._hand.classList.add('drag-block__hand');
+        this._container.appendChild(this._hand);
+
+        this._hand.onmousedown = this._handDown.bind(this);
+    };
+
+    App_DragBlock.prototype._handDown = function _handDown (e) {
+            var this$1 = this;
+
+        var width = parseInt(getComputedStyle(this._el).width,10);
+
+        var x1 = e.pageX;
+
+        document.body.classList.add('noselect');
+        document.body.onmousemove = function (e) {
+            var w = width - (x1 - e.pageX);
+
+            if (w < 0.1 * this$1._maxWidth) { w = Math.round(this$1._maxWidth * 0.1); }
+
+            if (w > this$1._maxWidth) { w = this$1._maxWidth; }
+
+            this$1._el.style.width = w + 'px';
+
+            if (this$1._config.callback) {
+                this$1._config.callback();
+            }
+        };
+
+        document.body.onmouseup = function () {
+            document.body.classList.remove('noselect');
+            document.body.onmouseup = document.body.onmousemove = null;
+        };
+    };
+
+    var App = function App () {};
+
+    App.DragBlock = App_DragBlock;
+
+    return App;
+
+}());
+//# sourceMappingURL=app.js.map
